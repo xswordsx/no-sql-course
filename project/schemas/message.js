08ba473c21c61f94098c2b0b239c8e780ca7a741
Ivan Latunov <xswordsx@gmail.com>
2015-08-22 16:53:03 +0300

    Add Schemas

---
+++ project/schemas/message.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mognoose.Schema;
+
+function shortMessage  (x) { return x.length <= 140 }
+function shortLocation (x) { return x.length <= 30 }
+
+var messageSchema = new Schema({
+	author: { type: Schema.Types.ObjectId, index: true, ref: 'User' },
+	content: { type: String, validate: [shortMessage, 'Message too long' ] },
+	location: { type: String, validate: [shortLocation, 'Location too long' ] },
+	published: { type: Date, default: Date.now },
+	tags: { type: [String], index: true }
+});
+
+exports = messageSchema;
---
+++ project/schemas/user.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+	username: { type: String, unique: true, index: true },
+	email: { type: String, unique: true },
+	password: String,
+	created: { type: Date, default: Date.now },
+	following: [ { type: Schema.Types.ObjectId, ref: 'User' } ],
+	verified: Boolean
+});
+
+exports = userSchema;
