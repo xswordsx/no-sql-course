fe47babdd064294e06ac34c4e000e05df337666f
Ivan Latunov <xswordsx@gmail.com>
2015-08-25 11:00:19 +0300

    Add solution to task 3

--- task-3/group-A/solution.js
+++ task-3/group-A/solution.js
@@ -21,10 +21,37 @@ mongoose.connect('mongodb://localhost:27017/jokes', function(err, ok) {
     done(-1);
   } else {
     console.log('connected to db');
-    mapReduceMails();
+    aggregate();
   }
 
 });
+
+function aggregate() {
+
+  var callback = function(err, result) {
+    if(err) {
+      console.error(err);
+      done(-3);
+    } else {
+      console.log('Character info saved to `top_3_jokes`');
+			done(0);
+    }};
+
+  Joke.aggregate([
+    { $unwind: "$characters" },
+    { $group: {
+      _id: "$characters",
+			text: { $addToSet: "$text" },
+			author: { $addToSet: "$author" },
+			jokes_count: { $sum: 1 },
+      avg_rating: { $avg: "$rating" }
+  	}},
+		{ $sort: { jokes_count: -1 } },
+		{ $limit: 3 },
+		{ $out: 'top_3_jokes' }
+  ], callback);
+
+}
 
 function mapReduceMails() {
   var o = {};
