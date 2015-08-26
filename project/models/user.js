a734402cb7bce86acd271b4e13b29f2563ac28aa
Ivan Latunov <xswordsx@gmail.com>
2015-08-26 22:12:46 +0300

    Reindent user model

--- project/models/user.js
+++ project/models/user.js
@@ -2,10 +2,25 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-	username: { type: String, unique: true, index: true, required: true },
-	email: { type: String, unique: true, required: true	 },
-	password: { type: String, required: true },
-	created: { type: Date, default: Date.now },
+	username: {
+		type: String,
+		unique: true,
+		index: true,
+		required: true
+	},
+	email: {
+		type: String,
+		unique: true,
+		required: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	created: {
+		type: Date,
+		default: Date.now
+	},
 	following: [ { type: Schema.Types.ObjectId, ref: 'User' } ],
 	verified: Boolean
 });
